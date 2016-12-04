e3218e476e1c05bdbd47853ee3b9a5537fa60a44
Mohamed Hegazy <mhegazy@microsoft.com>
2016-12-03 21:49:57 -0800

    Customize plugins for TypeScript playground

--- metadata.js
+++ metadata.js
@@ -18,38 +18,6 @@
 				npm: 'node_modules/monaco-typescript/release',
 				dev: '/monaco-typescript/out'
 			}
-		},{
-			name: 'monaco-css',
-			contrib: 'vs/language/css/monaco.contribution',
-			modulePrefix: 'vs/language/css',
-			paths: {
-				npm: 'node_modules/monaco-css/release/min',
-				dev: '/monaco-css/release/dev'
-			}
-		},{
-			name: 'monaco-json',
-			contrib: 'vs/language/json/monaco.contribution',
-			modulePrefix: 'vs/language/json',
-			paths: {
-				npm: 'node_modules/monaco-json/release/min',
-				dev: '/monaco-json/release/dev'
-			}
-		},{
-			name: 'monaco-html',
-			contrib: 'vs/language/html/monaco.contribution',
-			modulePrefix: 'vs/language/html',
-			paths: {
-				npm: 'node_modules/monaco-html/release/min',
-				dev: '/monaco-html/release/dev'
-			}
-		},{
-			name: 'monaco-languages',
-			contrib: 'vs/basic-languages/src/monaco.contribution',
-			modulePrefix: 'vs/basic-languages',
-			paths: {
-				npm: 'node_modules/monaco-languages/release',
-				dev: '/monaco-languages/out'
-			}
 		}]
 	}
 
