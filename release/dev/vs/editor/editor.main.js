8c59bd6fd6e3152419a09cad53d92d9b4eee4525
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2019-02-06 15:05:20 -0800

    Update to 3.3.1

--- release/dev/vs/editor/editor.main.js
+++ release/dev/vs/editor/editor.main.js
@@ -83835,7 +83835,7 @@ define(__m[505/*vs/editor/editor.main*/], __M([1/*require*/,0/*exports*/,160/*vs
 
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-typescript version: 2.0.1(a3d820dae06cf39ae34433092e561f7e332d16f5)
+ * monaco-typescript version: 2.0.1(50bcd692c552179324c50660082c23a306e249fb)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-typescript/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
--- release/min/vs/editor/editor.main.js
+++ release/min/vs/editor/editor.main.js
@@ -78,7 +78,7 @@ t instanceof D.OneReference&&(e._revealReference(t),e._onDidSelectReference.fire
 
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-typescript version: 2.0.1(a3d820dae06cf39ae34433092e561f7e332d16f5)
+ * monaco-typescript version: 2.0.1(50bcd692c552179324c50660082c23a306e249fb)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-typescript/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
